a878a8b3b7aa8cf44f5084fe03841a6615f71831
fengwangren <2629487604@qq.com>
2024-09-09 19:54:31 +0800

    test user/dome

--- router/user.go
+++ router/user.go
@@ -1,16 +1,31 @@
 package router
 
 import (
+	"fmt"
 	"github.com/dotdancer/gogofly/api"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitUserRouter() {
 	RegisterRouter(func(rgPublic *gin.RouterGroup, rgAuth *gin.RouterGroup) {
 		userApi := api.UserInfoApi{}
 		rgPublicUser := rgPublic.Group("/user")
+		rgPublicUser.Use(func(c *gin.Context) {
+			header := c.GetHeader("x-apikey")
+			fmt.Println("header:", header)
+			c.Next()
+		})
 		{
 			rgPublicUser.POST("/login", userApi.Login)
+			rgPublicUser.GET("/dome", func(c *gin.Context) {
+				c.AbortWithStatusJSON(http.StatusOK, gin.H{
+					"msg": "登录页面", "url": "/login.html",
+					"data": gin.H{
+						"username": "admin", "password": "123456",
+					},
+				})
+			})
 		}
 
 		rgAuthUser := rgAuth.Group("/user")
